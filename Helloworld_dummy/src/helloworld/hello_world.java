5e40288363662716c40b7bec795a33cef2a89038
John Slevinsky <john.slev@gmail.com>
2013-11-03 16:52:24 -0700

    Hello world test

---
+++ Helloworld_dummy/src/helloworld/hello_world.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		
+
+	}
+
+}
