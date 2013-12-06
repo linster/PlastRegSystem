c68f7c54fad2276d361bb3d1d6b90e4c028c9224
Stefan Martynkiw <linster247@gmail.com>
2013-12-05 23:51:45 -0700

    More fiddling to get the button click handler working. When I got bored of that, I tried to center align the login form on the page.

--- dt/src/com/example/dt/LoginView.java
+++ dt/src/com/example/dt/LoginView.java
@@ -1,10 +1,14 @@
 package com.example.dt;
 
+import java.io.File;
+
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.FileResource;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.FormLayout;
+import com.vaadin.server.VaadinService;
 import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
 
 public class LoginView extends Panel implements View {
 
@@ -24,10 +28,25 @@ public class LoginView extends Panel implements View {
 	Button blogin = null;
 	
 	private void initlayout() {
-		setSizeFull();
+		
+		
+
 		//New FormLayout to hold Username / Password fields
 		FormLayout flogin = new FormLayout();
-		setContent(flogin);
+		
+		
+		//Need to create a layout for our Logo to live in.
+		HorizontalLayout logolayout = new HorizontalLayout();
+		logolayout.setHeight("100px");
+		FileResource rlogoimage = new FileResource( new File((VaadinService.getCurrent().getBaseDirectory().getAbsolutePath())+"/WEB-INF/images/logo.png" ));
+		Image logoimage = new Image("", rlogoimage);
+		logoimage.setHeight("80px");
+		logolayout.addComponent(logoimage);
+		Label main = new Label("<h1>PlastOnline</h1>",Label.CONTENT_XHTML);
+		logolayout.addComponent(main);
+		logolayout.setComponentAlignment(main, Alignment.BOTTOM_CENTER );
+		
+		flogin.addComponent(logolayout);
 		
 		tfusername = new TextField("Username");
 		tfpassword = new PasswordField("Password");
@@ -41,6 +60,23 @@ public class LoginView extends Panel implements View {
 		blogin = new Button("Login");
 		flogin.addComponent(blogin);
 		
+		//Make a Vertical Layout to house the form layout
+		//So that we can center/center align the formlayout in the page
+		VerticalLayout vl = new VerticalLayout();
+		vl.setSizeFull();
+		vl.addComponent(flogin);
+		vl.setComponentAlignment(flogin, Alignment.TOP_RIGHT);
+		setContent(vl);
+		
+		
+		//BAD BAD BAD CODE
+		blogin.addClickListener(new Button.ClickListener() { 
+			public void buttonClick(ClickEvent event) {
+				Notification.show("Button clicked");
+				MasterNavigator.getInstance().getNav().navigateTo("Main");
+			}
+		} );
+
 	}
 
 }
