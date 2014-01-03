414736e02bbe7ae24385c0f28d74c3382320a831
John Slevinsky <john.slev@gmail.com>
2014-01-03 16:11:05 -0700

    Put label in onlineaccountinfo. Doesn't acctually work.

--- src/org/plast/reg/ui/MyAccount_OnlineInformation_View.java
+++ src/org/plast/reg/ui/MyAccount_OnlineInformation_View.java
@@ -3,19 +3,28 @@ package org.plast.reg.ui;
 import com.google.common.eventbus.EventBus;
 import com.vaadin.ui.*;
 
-public class MyAccount_OnlineInformation_View extends MainShellView {
+public class MyAccount_OnlineInformation_View extends MainShellView
+	{
 
-	public MyAccount_OnlineInformation_View(EventBus authBus, String pageTitle) {
-		super(authBus, pageTitle);
+	public MyAccount_OnlineInformation_View(EventBus authBus, String pageTitle)
+		{
+			super(authBus, pageTitle);
 		
 		
-		//Set the main application component to be a label.
-		//In the future, we'll probably define a FormLayout, pack a bunch of fields into it, and then add the layout into the line below.
-		//super.setApplicationComponent(new Label("Online Account Manager"));
-		super.apppanel.setContent(new Label("Test"));
-		
-	}
-	
+
+			
+			VerticalLayout layout = new VerticalLayout();
+			
+			Label label = new Label ("Thisis a cool label");
+			layout.addComponent(label);
+			
+			
+			//Set the main application component to be a label.
+			//In the future, we'll probably define a FormLayout, pack a bunch of fields into it, and then add the layout into the line below.
+			//super.setApplicationComponent(new Label("Online Account Manager"));
+
+			apppanel.setContent(layout);
+		}
 	
 
 }
